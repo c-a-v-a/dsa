f67d4a7bf12e64941787078ca1cb7799becff1a9
c-a-v-a <cavaevilmail@gmail.com>
2025-01-10 21:25:15 +0100

    ...

--- dsa.py
+++ dsa.py
@@ -1,15 +1,19 @@
 from random import getrandbits, randrange
 from sympy import isprime
+from hashlib import sha256
 
 P_BIT_LENGTH = 2048
 Q_BIT_LENGTH = 256
 
 class DSA:
   def __init__(self):
+    # Params
     self.p = 0
     self.q = 0
     self.g = 0
+    # Private key
     self.x = 0
+    # Public key
     self.y = 0
 
   def keygen(self):
@@ -39,7 +43,10 @@ class DSA:
 
   def save_keys(self, private_key_path, public_key_path):
     f = open(private_key_path, 'w')
-    f.write(str(self.x))
+    f.write(f'{self.p}\n')
+    f.write(f'{self.q}\n')
+    f.write(f'{self.g}\n')
+    f.write(f'{self.x}\n')
     f.close()
 
     f = open(public_key_path, 'w')
@@ -49,16 +56,59 @@ class DSA:
     f.write(f'{self.y}\n')
     f.close()
 
-  def load_keys(self, private_key_path, public_key_path):
-    f = open(private_key_path, 'r')
-    self.x = int(f.readline())
+  def load_private_key(self, path):
+    f = open(path, 'r')
+    lines = f.readlines()
+    ints = list(map(int, lines))
+    [self.p, self.q, self.g, self.x] = ints
     f.close()
 
-    f = open(public_key_path, 'r')
+  def load_public_key(self, path):
+    f = open(path, 'r')
     lines = f.readlines()
     ints = list(map(int, lines))
     [self.p, self.q, self.g, self.y] = ints
     f.close()
+
+  def sign(self, message, signature_path):
+    sha = sha256(message.encode()).hexdigest()
+    H = int(sha, 16)
+    s = 0
+    r = 0
+
+    while s == 0 or r == 0:
+      k = randrange(1,self.q)
+      invk = pow(k, -1, self.q)
+      r = pow(self.g,k,self.p) % self.q
+      s = H + self.x*r
+      s = (s * invk) % self.q
+
+    print(r, s)
+
+    f = open(signature_path, 'w')
+    f.write(f'{r}\n')
+    f.write(f'{s}\n')
+    f.close()
+
+  def validate(self, message, signature_path):
+    sha = sha256(message.encode()).hexdigest()
+    H = int(sha, 16)
+
+    f = open(signature_path, 'r')
+    lines = f.readlines()
+    ints = list(map(int, lines))
+    [r, s] = ints
+    f.close()
+
+    w = pow(s, -1, self.q)
+    v1 = (H * w) % self.q
+    v2 = (r * w) % self.q
+    v = pow(self.g, v1, self.p)
+    v = v * pow(self.y, v2, self.p)
+    v = v % self.p
+    v = v % self.q
+
+    return v == r
 
   def _rand_prime(self, bit_length):
     while True:
